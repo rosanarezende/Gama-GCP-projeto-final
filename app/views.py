7c775c0cceba9f5a13ad1949d2609b18e0eb1001
Rosana Rezende <rezende_rosana@hotmail.com>
2021-09-25 15:56:18 -0300

    lista empresas

--- app/views.py
+++ app/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from app import models, forms
+
 
 def home(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
+
+def listarEmpresas(request):
+    data = {}
+    buscarNome = request.GET.get('buscar-nome')
+    if buscarNome:
+        data['empresas'] = models.Empresas.objects.filter(nome__icontains = buscarNome)
+    else: 
+        data['empresas'] = models.Empresas.objects.all()
+    return render(request,"empresas.html", data)
